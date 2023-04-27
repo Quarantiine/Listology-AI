d70adebf91de707bdd90004263875cecc4db38f1
Daniel Ward <02danwar.buss@gmail.com>
2023-04-27 02:06:04 -0500

    Huge changes

---
+++ src/pages/api/firebaseApi.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyDetapdvyXWk9GParxqPi1YudSyCOnSbyc",
+	authDomain: "todo-list-web-app-3329a.firebaseapp.com",
+	projectId: "todo-list-web-app-3329a",
+	storageBucket: "todo-list-web-app-3329a.appspot.com",
+	messagingSenderId: "26673966045",
+	appId: "1:26673966045:web:22be18e3d54b36ba03e434",
+	measurementId: "G-PBC8GGMJYC",
+};
+
+export default function firebaseApi() {
+	return {};
+}
